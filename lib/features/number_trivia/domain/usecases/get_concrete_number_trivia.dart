918ef6910e2cb2dbb0b2aa325885b83cf7c6cbad
Piotr <piotrekert90@gmail.com>
2020-03-14 20:45:14 +0100

    Add GetConcreteNumberTrivia usecase. Add .gitkeep files for test package.

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,7 @@
+import 'package:number_trivia/features/number_trivia/domain/repositiories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+}
