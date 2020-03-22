1c4b614b3c3878d836a9de5339032e7b9b5a17cd
Piotr <piotrekert90@gmail.com>
2020-03-22 12:59:37 +0100

    Update number_trivia_state.dart

--- lib/features/number_trivia/presentation/bloc/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/bloc/number_trivia_bloc.dart
@@ -3,13 +3,14 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   @override
-  NumberTriviaState get initialState => NumberTriviaInitial();
+  NumberTriviaState get initialState => Empty();
 
   @override
   Stream<NumberTriviaState> mapEventToState(
--- lib/features/number_trivia/presentation/bloc/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/bloc/number_trivia_state.dart
@@ -1,6 +1,22 @@
 part of 'number_trivia_bloc.dart';
 
 @immutable
-abstract class NumberTriviaState {}
+abstract class NumberTriviaState extends Equatable {
+  NumberTriviaState([List props = const <dynamic>[]]) : super(props);
+}
 
-class NumberTriviaInitial extends NumberTriviaState {}
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({@required this.trivia}) : super([trivia]);
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({@required this.message}) : super([message]);
+}
