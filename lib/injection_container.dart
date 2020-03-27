0a1e87adf1db6f719952491e62b1be43f93d8113
Piotr <piotrekert90@gmail.com>
2020-03-27 21:02:10 +0100

    Register NumberTriviaRepository instance as singleton.

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,8 @@
 import 'package:get_it/get_it.dart';
 
 import 'core/util/input_converter.dart';
+import 'features/number_trivia/data/repositiories/number_trivia_repository_impl.dart';
+import 'features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
@@ -17,12 +19,21 @@ void init() {
       inputConverter: sl(),
     ),
   );
+  
+  // Repository
+  sl.registerLazySingleton<NumberTriviaRepository>(
+    () => NumberTriviaRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
 
-// Use cases
+  // Use cases
   sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
   sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
 
-//! Core
+  //! Core
   sl.registerLazySingleton(() => InputConverter());
 
   //! External
