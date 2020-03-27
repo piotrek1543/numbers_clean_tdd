35539325948018bf3a6e9818cddfd4e431b10bce
Piotr <piotrekert90@gmail.com>
2020-03-27 20:46:48 +0100

    Register NumberTriviaBloc factory using get_it.

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,9 +1,19 @@
 import 'package:get_it/get_it.dart';
 
+import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+
 final sl = GetIt.instance;
 
 void init() {
   //! Features - Number Trivia
+  //Bloc
+  sl.registerFactory(
+  () => NumberTriviaBloc(
+    concrete: sl(),
+    random: sl(),
+    inputConverter: sl(),
+  ),
+);
 
   //! Core
 
