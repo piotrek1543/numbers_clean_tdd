0212e346ea26fd0a1f63c545671989264111ee74
Piotr <piotrekert90@gmail.com>
2020-03-27 20:58:46 +0100

    Register usecases and InputConverter as singletons.

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,8 @@
 import 'package:get_it/get_it.dart';
 
+import 'core/util/input_converter.dart';
+import 'features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 
 final sl = GetIt.instance;
@@ -8,15 +11,19 @@ void init() {
   //! Features - Number Trivia
   //Bloc
   sl.registerFactory(
-  () => NumberTriviaBloc(
-    concrete: sl(),
-    random: sl(),
-    inputConverter: sl(),
-  ),
-);
+    () => NumberTriviaBloc(
+      concrete: sl(),
+      random: sl(),
+      inputConverter: sl(),
+    ),
+  );
 
-  //! Core
+// Use cases
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
+
+//! Core
+  sl.registerLazySingleton(() => InputConverter());
 
   //! External
-
 }
