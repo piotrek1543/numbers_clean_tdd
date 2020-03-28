3807a9a42563b1b8abb4a4b48a9e5b7fddf4be49
Piotr <piotrekert90@gmail.com>
2020-03-28 11:16:40 +0100

    Create widgets.dart. Update number_trivia_page.dart.

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
-import 'package:number_trivia/features/number_trivia/presentation/widgets/loading_widget.dart';
-import 'package:number_trivia/features/number_trivia/presentation/widgets/message_display.dart';
+import 'package:number_trivia/features/number_trivia/presentation/widgets/widgets.dart';
 import 'package:number_trivia/injection_container.dart';
 
 class NumberTriviaPage extends StatelessWidget {
@@ -34,6 +33,10 @@ class NumberTriviaPage extends StatelessWidget {
                     );
                   } else if (state is Loading) {
                     return LoadingWidget();
+                  } else if (state is Loaded) {
+                    return TriviaDisplay(
+                      numberTrivia: state.trivia,
+                    );
                   } else if (state is Error) {
                     return MessageDisplay(
                       message: state.message,
