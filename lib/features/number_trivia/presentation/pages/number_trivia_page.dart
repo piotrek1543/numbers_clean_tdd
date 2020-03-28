a10879f021de34d25f2dc5bbb963e6f95d2b89c5
Piotr <piotrekert90@gmail.com>
2020-03-28 08:21:17 +0100

    Build basic UI outline using Placeholder widgets.

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -10,9 +10,51 @@ class NumberTriviaPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Number Trivia'),
       ),
-      body: BlocProvider(
-        builder: (_) => sl<NumberTriviaBloc>(),
-        child: Container(),
+      body: _buildBody(context),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc> _buildBody(BuildContext context) {
+    return BlocProvider(
+      builder: (_) => sl<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 10),
+              // Top half
+              Container(
+                // Third of the size of the screen
+                height: MediaQuery.of(context).size.height / 3,
+                // Message Text widgets / CircularLoadingIndicator
+                child: Placeholder(),
+              ),
+              SizedBox(height: 20),
+              // Bottom half
+              Column(
+                children: <Widget>[
+                  // TextField
+                  Placeholder(fallbackHeight: 40),
+                  SizedBox(height: 10),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        // Search concrete button
+                        child: Placeholder(fallbackHeight: 30),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        // Random button
+                        child: Placeholder(fallbackHeight: 30),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
